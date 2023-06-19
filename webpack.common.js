d83f3d50bfcf74401ce07ec1b6101de2043649e6
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-06-19 16:59:11 +0300

    Split Webpack config, added Webpack server

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,7 @@
-const path = require("path");
-
 let HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
     entry: "./src/index.js",
-    output: {
-        filename: "main.[hash].js",
-        path: path.resolve(__dirname, "dist")
-    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html"
