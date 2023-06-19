2b234efb623948d5bb936ee22cac66470a0185ce
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-06-19 10:11:32 +0300

    Added imports, exports, switched to webpack build

--- src/app/alert.service.js
+++ src/app/alert.service.js
@@ -1,4 +1,6 @@
-class AlertService {
+import {inputsAreValid} from "./utils/inputs-valid";
+
+export class AlertService {
     constructor() {
         this.errorBox = document.getElementById("error");
     }
