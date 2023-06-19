f91ecaaed8e163d321827013bed2351b1287bc24
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-06-19 18:22:41 +0300

    Added clean webpack plugin, asset modules

--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,9 @@ let HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: "./src/index.js",
+    output: {
+        assetModuleFilename: 'images/[name].[hash].[ext]' // Starting from Webpack 5
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: "./src/template.html"
@@ -16,6 +19,16 @@ module.exports = {
                     "css-loader", // 2. Turns css into js
                     "sass-loader" // 1. Turns scss into css
                 ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    "html-loader"
+                ]
+            },
+            {
+                test: /\.(svg|png|jpg|gif)$/,
+                type: "asset/resource", // Starting from Webpack 5
             }
         ]
     }
