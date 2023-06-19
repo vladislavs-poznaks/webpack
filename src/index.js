b93c618616d0bee3abb640493d30e8c308af1ea2
Vladislavs Poznaks <vladislavs.poznaks@gmail.com>
2023-06-19 16:22:24 +0300

    Added html template

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ import {ComponentService} from "./app/component.service";
 const alertService = new AlertService();
 const componentService = new ComponentService();
 
+console.log("Hi!");
+
 run(alertService, componentService);
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const path = require("path");
 
+let HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
     mode: "development",
     entry: "./src/index.js",
     output: {
-        filename: "main.js",
+        filename: "main.[hash].js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/template.html"
+        }),
+    ],
     module: {
         rules: [
             {
